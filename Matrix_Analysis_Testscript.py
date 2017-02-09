fixed eigenvector representation and calculation of angles

Matrix_Analysis_Testscript.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import imp
 from elephant.spike_train_surrogates import *
 from quantities import Hz, ms
+from scipy.linalg import eigh
 
 
 COLLAB_PATH = '/home/robin/NeuroSim-Comparison-Tools'
@@ -42,7 +43,7 @@ def analyze_spiketrains(spiketrain_list, filename='testfile'):
     # Generate Surrogate Correlation Matrix
     surrogate_corr_matrix = matstat.corr_matrix(surrogate_spiketrain_list)
 
-
+    # Crate Figure
     fig, ax = plt.subplots(nrows=2, ncols=2)
     fig.tight_layout()
 
@@ -53,66 +54,44 @@ def analyze_spiketrains(spiketrain_list, filename='testfile'):
     matstat.plot_matrix(corr_matrix, ax[0,1])
 
     # EW Spectra
-    EWs, EVs = np.linalg.eigh(corr_matrix)
-    sEWs, sEVs = np.linalg.eigh(surrogate_corr_matrix)
+    EWs, EVs = eigh(corr_matrix)
+    sEWs, sEVs = eigh(surrogate_corr_matrix)
     matstat.eigenvalue_distribution(EWs, ax[1,0], surrogate_EWs=sEWs,
                                     binnum=int(max(EWs))*5)
-    matstat.pc_trafo(corr_matrix)
 
-    # EW redundancy
     matstat.redundancy(EWs)
 
     # EW significance
     PCs = matstat.nbr_of_pcs(EWs, method='SCREE', alpha=.05, ax=ax[1,1])
     pc_count = len(PCs)
 
-    # Print to file
-    import scipy.io
-    scipy.io.savemat("a.mat", {"corrmatrix": corr_matrix, "ew": EWs, "ev": EVs})
-    N = len(EWs)
-    f = open(filename,'w+')
-    print >>f, "Correlation Matrix ({}x{}): \n".format(N, N)
-    print >>f, corr_matrix
-    print >>f, "\nEigenvalues:\n"
-    print >>f, EWs
-    print >>f, "\nEigenvectors:\n"
-    print >>f, EVs
-
     return EWs, EVs, pc_count
 
 
 # Generate Spiketrains
 N = 30
-spiketrain_list = testdata.test_data(size=N, corr=.7, t_stop=500*ms,
-                                     rate=100*Hz, assembly_sizes=[10, 5],
-                                     method="CPP", bkgr_corr=0.01)
+spiketrain_list = testdata.test_data(size=N, corr=[.9,.9,.9], t_stop=500*ms,
+                                     rate=100*Hz, assembly_sizes=[8,8,8],
+                                     method="CPP", bkgr_corr=0.9)
 for i, st in enumerate(spiketrain_list):
     st.annotations['id'] = i
 
 EWs1, EVs1, pc_count1 = analyze_spiketrains(spiketrain_list)
 
 # Generate second dataset
-spiketrain_list = testdata.test_data(size=N, corr=.7, t_stop=100*ms,
-                                     rate=100*Hz, assembly_sizes=[5],
-                                     method="CPP", bkgr_corr=0)
+spiketrain_list = testdata.test_data(size=N, corr=[.9,.9,.9], t_stop=500*ms,
+                                     rate=100*Hz, assembly_sizes=[8,8,8],
+                                     method="CPP", bkgr_corr=0.9)
 
-# EWs2, EVs2, pc_count2 = analyze_spiketrains(spiketrain_list)
+EWs2, EVs2, pc_count2 = analyze_spiketrains(spiketrain_list)
 
 # Angles between eigenspaces
-# assemblysize = int(max([max(EWs1), max(EWs2)]))
-# matstat.EV_angles(EVs1[:assemblysize], EVs2[:assemblysize])
-
-# The eigenvectors with the smallest eigenvalues point to the assembly
-# neurons because the assemblies with high correlation show the least variance.
-# Therefore the relevant angels are of EV[:-assemblysize]
-# The assemblysizes can be estimate by the size of the eigenvalues
+assemblysize = pc_count1
+assemblysize = N
+matstat.EV_angles(EVs1[:, -assemblysize:], EVs2[:, -assemblysize:])
 
 # Print eigenvectors
-matstat.print_eigenvectors(EVs1[::-1], EWs1[::-1])
-# matstat.print_eigenvectors(EVs2[::-1], EWs2[::-1])
+matstat.print_eigenvectors(EVs1, EWs1)
+matstat.print_eigenvectors(EVs2, EWs2)
 
-plt.show()
-
-# ToDo: Print results of organized test runs into file for further analysis
-# Covariance matrix
-# vergleich zu matlab
+# plt.show()

validation/matrix.py
@@ -102,11 +102,12 @@ def eigenvalue_distribution(EWs, ax=plt.gca(), binnum=20, surrogate_EWs=None):
         wigner_y = [wigner_dist(x) for x in wigner_x]
         ax.plot(wigner_x, wigner_y, color='r')
 
+    nbr_of_significant_ew = len(np.where(EWs > max(surrogate_EWs))[0])
     ax.set_xlabel('EW')
     ax.set_ylabel('Occurrence')
     print "\t{} eigenvalues are larger than the reference distribution \n"\
-          .format(len(np.where(EWs > max(surrogate_EWs))[0]))
-    return None
+          .format(nbr_of_significant_ew)
+    return nbr_of_significant_ew
 
 
 def redundancy(EWs):
@@ -207,27 +208,29 @@ def print_eigenvectors(EVs, EWs=[], min=0, max=1.,
     if len(EWs) == 0:
         EWs = np.arange(len(EVs))
 
-    for i, ev in enumerate(EVs):
-        print "\033[47m\033[30m{:2.0f}:\033[0m ".format(EWs[i]),
-        for e in ev:
-            print "\033[{}m {:+.2f}".format(colormap[colorcode(e)], e),
+    for i, EV in enumerate(EVs.T[::-1]):
+        print "\033[47m\033[30m{:3.1f}:\033[0m\t".format(EWs[-(i+1)]),
+        np.testing.assert_almost_equal(np.linalg.norm(EV), 1., decimal=7)
+        for n_coord in EV:
+            print "\033[{}m {:+.2f}"\
+                .format(colormap[colorcode(n_coord)], n_coord),
         print " "
     return None
 
 
 def EV_angles(EVs1, EVs2, deg=True):
-    ### EVs must be sorted
+    ### EVs must be sorted in ascending order as returned by scipy.linalg.eigh()
     assert len(EVs1) == len(EVs2)
-    for EV in [EVs1, EVs2]:
-        assert np.all(np.array([np.linalg.norm(ev) for ev in EV])) == 1.,\
-               "The eigenvectors are not normalized!"
+    # Transform into descending array of the eigenvector arrays
+    EVs1 = np.absolute(EVs1.T[::-1])
+    EVs2 = np.absolute(EVs2.T[::-1])
+    for EVs in [EVs1, EVs2]:
+        for EV in EVs:
+            np.testing.assert_almost_equal(np.linalg.norm(EV), 1., decimal=7)
 
-    N = len(EVs1)
-    EV_angles = np.array([np.arccos(np.dot(ev1, ev2))
-                          for (ev1, ev2) in zip(EVs1, EVs2)])
-
-    M = np.dot(EVs1, np.transpose(EVs2))
-    space_angle = np.arccos(np.sqrt(np.linalg.det(np.dot(M, np.transpose(M)))))
+    M = np.dot(EVs1, EVs2.T)
+    EV_angles = np.arccos(np.diag(M))
+    space_angle = np.arccos(np.sqrt(np.linalg.det(np.dot(M, M.T))))
 
     if deg:
         EV_angles *= 180 / np.pi

validation/test_data.py
@@ -43,11 +43,15 @@ def test_data(size, corr, t_stop, rate, method="CPP", assembly_sizes=[1],
               bkgr_corr=0., sorted=False):
 
     spiketrains = [None] * size
+    if not type(corr) == list:
+        corr = [corr] * len(assembly_sizes)
+    elif len(corr) == 1:
+        corr *= len(assembly_sizes)
 
     for i, a_size in enumerate(assembly_sizes):
         generated_sts = int(np.sum(assembly_sizes[:i]))
         spiketrains[generated_sts:generated_sts+a_size]\
-            = generate_assembly(a_size, corr, method, rate, t_stop)
+            = generate_assembly(a_size, corr[i], method, rate, t_stop)
 
     if bkgr_corr > 0:
         bkgr_size = size-sum(assembly_sizes)+1
@@ -59,7 +63,6 @@ def test_data(size, corr, t_stop, rate, method="CPP", assembly_sizes=[1],
         spiketrains[np.sum(assembly_sizes):] \
             = np.array([HPP(rate=rate, t_stop=t_stop)
                         for x in range(size-sum(assembly_sizes))])
-    # Background Correlation
     return spiketrains
 
 